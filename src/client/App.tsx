af512437a583f9220bd8f0a049b87465843a5f94
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-07-26 12:48:26 -0400

    initialize react-query in app component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -9,6 +9,7 @@ import TeacherSignup from "../pages/TeacherSignup";
 import TeacherHome from "../pages/TeacherHome";
 import { UserContext } from "./UserContext";
 import TeacherBooking from "../pages/TeacherBooking";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 function App() {
   const [userData, setUserData] = useState({
@@ -24,8 +25,11 @@ function App() {
     setUserData(prevData => ({...prevData, ...newData}))
   }
 
+  const client = new QueryClient();
+
   return (
     <UserContext.Provider value={{userData, updateUserData}}>
+      <QueryClientProvider client={client}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -38,6 +42,7 @@ function App() {
           <Route path="/teachers/:id" element={<TeacherBooking/>}/>
         </Routes>
       </Router>
+      </QueryClientProvider>
     </UserContext.Provider>
   );
 }
