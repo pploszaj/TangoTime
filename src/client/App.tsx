6d2fc0c9b34bf780cc6002c8474b86c643d6e099
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-08-04 11:42:55 -0400

    add code splitting with react.lazy and suspense

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Login from "../pages/Login";
-import SelectRole from "../pages/SelectRole";
-import Signup from "../pages/Signup";
-import StudentHome from "../pages/StudentHome";
-import TeacherSignup from "../pages/TeacherSignup";
-import TeacherHome from "../pages/TeacherHome";
+// import Login from "../pages/Login";
+// import SelectRole from "../pages/SelectRole";
+// import Signup from "../pages/Signup";
+// import StudentHome from "../pages/StudentHome";
+// import TeacherSignup from "../pages/TeacherSignup";
+// import TeacherHome from "../pages/TeacherHome";
+// import TeacherBooking from "../pages/TeacherBooking";
 import { UserContext } from "./UserContext";
-import TeacherBooking from "../pages/TeacherBooking";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const Login = lazy(() => import("../pages/Login"));
+const SelectRole = lazy(() => import("../pages/SelectRole"));
+const Signup = lazy(() => import("../pages/Signup"));
+const StudentHome = lazy(() => import("../pages/StudentHome"));
+const TeacherSignup = lazy(() => import("../pages/TeacherSignup"));
+const TeacherHome = lazy(() => import("../pages/TeacherHome"));
+const TeacherBooking = lazy(() => import("../pages/TeacherBooking"));
 
 function App() {
   const [userData, setUserData] = useState({
@@ -38,16 +46,18 @@ function App() {
     <UserContext.Provider value={{ userData, updateUserData }}>
       <Router>
         <QueryClientProvider client={client}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/role" element={<SelectRole />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/teachersignup" element={<TeacherSignup />} />
-            <Route path="/studenthome" element={<StudentHome />} />
-            <Route path="/teacherhome" element={<TeacherHome />} />
-            <Route path="/teachers/:id" element={<TeacherBooking />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/role" element={<SelectRole />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/teachersignup" element={<TeacherSignup />} />
+              <Route path="/studenthome" element={<StudentHome />} />
+              <Route path="/teacherhome" element={<TeacherHome />} />
+              <Route path="/teachers/:id" element={<TeacherBooking />} />
+            </Routes>
+          </Suspense>
         </QueryClientProvider>
       </Router>
     </UserContext.Provider>
