062fe05ef4e36551c0113f8593da623681ee926d
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-09-05 15:48:34 -0400

    fix bus for deployment

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,52 +1,68 @@
-import express, { Request, Response, NextFunction } from 'express';
-import authenticationController from './controllers/authenticationController'
-
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import authenticationController from "./controllers/authenticationController";
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.post('/signup', authenticationController.createUser, (req,res) => {
-    res.json(res.locals.userid)
-})
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.post('/login', authenticationController.findUser, (req,res) => {
-    res.json(res.locals.userData)
-})
+app.post("/signup", authenticationController.createUser, (req, res) => {
+  res.json(res.locals.userid);
+});
 
-app.post('/teacherschedule', authenticationController.updateSchedule, (req,res) => {
-    res.json(res.locals.availabilities)
-})
+app.post("/login", authenticationController.findUser, (req, res) => {
+  res.json(res.locals.userData);
+});
 
-app.get('/teachers', authenticationController.findTeachers, (req,res) => {
-    res.json(res.locals.teachers)
-})
+app.post(
+  "/teacherschedule",
+  authenticationController.updateSchedule,
+  (req, res) => {
+    res.json(res.locals.availabilities);
+  }
+);
 
-app.post('/getTimes', authenticationController.getTimes, (req,res) => {
-    res.json(res.locals.times)
-})
+app.get("/teachers", authenticationController.findTeachers, (req, res) => {
+  res.json(res.locals.teachers);
+});
 
-app.post('/getBookings', authenticationController.getBookings, (req,res) => {
-    res.json(res.locals.bookings)
-})
+app.post("/getTimes", authenticationController.getTimes, (req, res) => {
+  res.json(res.locals.times);
+});
 
-app.post('/booking', authenticationController.bookLesson, authenticationController.sendTextMessage, (req,res) => {
-    console.log('Booking Successful!')
+app.post("/getBookings", authenticationController.getBookings, (req, res) => {
+  res.json(res.locals.bookings);
+});
+
+app.post(
+  "/booking",
+  authenticationController.bookLesson,
+  authenticationController.sendTextMessage,
+  (req, res) => {
+    console.log("Booking Successful!");
     res.json(res.locals.newBooking);
-})
+  }
+);
 
-app.use((err:any, req: Request, res: Response, next: NextFunction) => {
-    const defaultErr = {
-      log: 'Express error handler caught unknown middleware error',
-      status: 400,
-      message: { err: 'An error global occurred' },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
-  });
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
 
+// app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+//     console.error('Error occurred: ', err);
+
+//   const defaultErr = {
+//     log: "Express error handler caught unknown middleware error",
+//     status: 400,
+//     message: { err: "An error global occurred" },
+//   };
+//   const errorObj = Object.assign({}, defaultErr, err);
+//   console.log(errorObj.log);
+//   return res.status(errorObj.status).json(errorObj.message);
+// });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
